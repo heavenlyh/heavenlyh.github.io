197e03a85cbbfdbcb30b17e0b950da85589dd6cb
heavenlyh <79995639+heavenlyh@users.noreply.github.com>
2021-03-06 20:26:41 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -67,6 +67,20 @@ $('.carousel').owlCarousel({
         loop: true
     });
 
+    var typed = new Typed(".typingo ", {
+        strings: [ " &#8221; Небесна Сотня  &#8221; ?"],
+        typeSpeed: 150,
+        backSpeed: 150,
+        loop: true
+    });
+    
+    var typed = new Typed(".typingo-2 ", {
+        strings: [ " &#8221; Небесна Сотня  &#8221; ?"],
+        typeSpeed: 150,
+        backSpeed: 150,
+        loop: true
+    });
+
 
 });
 
