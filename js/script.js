5bc1666883a07e888b4aa498423c192f69b5218c
heavenlyh <79995639+heavenlyh@users.noreply.github.com>
2021-03-06 19:01:48 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -72,3 +72,9 @@ $('.carousel').owlCarousel({
         loop: true
     });
 });
+
+func handleLikes(w http.ResponseWriter, r *http.Request) {
+    // Do something meaningful with the request here...
+
+    // Return the HTTP status number 204.
+    w.WriteHeader(http.StatusNoContent)
