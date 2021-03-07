70428119a718e83662470ad35905209d48dfe13c
heavenlyh <79995639+heavenlyh@users.noreply.github.com>
2021-03-07 12:30:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,78 @@
 
 
+
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        $('html').css("scrollBehavior", "auto");
+    });
+    $('.navbar .menu li a').click(function(){
+        $('html').css("scrollBehavior", "smooth");
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+
+
+$('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 100,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
+
+
+    var typed = new Typed(".typing", {
+        strings: [ "2014"],
+        typeSpeed: 150,
+        backSpeed: 100,
+        loop: true
+    });
+
+
+    var typed = new Typed(".typing-2 ", {
+        strings: [ " &#8221; Небесна Сотня  &#8221; ?"],
+        typeSpeed: 150,
+        backSpeed: 150,
+        loop: true
+    });
+
+
+});
+
+
+
+
+
