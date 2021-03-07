6dcdfc6615b7a5133e7ba09e2067230013638d0e
heavenlyh <79995639+heavenlyh@users.noreply.github.com>
2021-03-07 12:24:20 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -80,31 +80,5 @@ $('.carousel').owlCarousel({
 });
 
 
-window.purechatApi = {
-	l: [],
-	t: [],
-	on: function() {
-		this.l.push(arguments);
-	}
-};
-(function() {
-	var done = false;
-	var script = document.createElement('script');
-	script.async = true;
-	script.type = 'text/javascript';
-	script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript';
-	document.getElementsByTagName('HEAD').item(0).appendChild(script);
-	script.onreadystatechange = script.onload = function(e) {
-		if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
-			var w = new PCWidget({
-				c: 'e566e524-5985-4737-aa9b-5a5313412358',
-				f: true
-			});
-			done = true;
-		}
-	};
-})();
-
-
 
 
